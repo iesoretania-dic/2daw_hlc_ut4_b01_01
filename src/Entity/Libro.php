92c0fe664b2d5580bef7ce18455af565e8533c06
Luis Ramón López López <lrlopez@gmail.com>
2020-12-09 13:54:26 +0100

    Implementados getters/setters de Libro

--- src/Entity/Libro.php
+++ src/Entity/Libro.php
@@ -53,4 +53,120 @@ class Libro
      * @var string
      */
     private $sinopsis;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitulo(): string
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * @param string $titulo
+     * @return Libro
+     */
+    public function setTitulo(string $titulo): Libro
+    {
+        $this->titulo = $titulo;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAutor(): string
+    {
+        return $this->autor;
+    }
+
+    /**
+     * @param string $autor
+     * @return Libro
+     */
+    public function setAutor(string $autor): Libro
+    {
+        $this->autor = $autor;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAnioPublicacion(): int
+    {
+        return $this->anioPublicacion;
+    }
+
+    /**
+     * @param int $anioPublicacion
+     * @return Libro
+     */
+    public function setAnioPublicacion(int $anioPublicacion): Libro
+    {
+        $this->anioPublicacion = $anioPublicacion;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIsbn(): string
+    {
+        return $this->isbn;
+    }
+
+    /**
+     * @param string $isbn
+     * @return Libro
+     */
+    public function setIsbn(string $isbn): Libro
+    {
+        $this->isbn = $isbn;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPaginas(): ?int
+    {
+        return $this->paginas;
+    }
+
+    /**
+     * @param int|null $paginas
+     * @return Libro
+     */
+    public function setPaginas(?int $paginas): Libro
+    {
+        $this->paginas = $paginas;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSinopsis(): string
+    {
+        return $this->sinopsis;
+    }
+
+    /**
+     * @param string $sinopsis
+     * @return Libro
+     */
+    public function setSinopsis(string $sinopsis): Libro
+    {
+        $this->sinopsis = $sinopsis;
+        return $this;
+    }
 }
